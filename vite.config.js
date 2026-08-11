9ede36461644940aa7b687c9d07e9b91e1a2431b
JesusGironVzla2 <167342025+JesusGironVzla2@users.noreply.github.com>
2026-08-11 17:16:40 -0400

    Convert app to PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['icon.svg'],
+      manifest: {
+        name: 'FitPro App',
+        short_name: 'FitPro',
+        description: 'Plataforma Inteligente para Entrenadores y Alumnos',
+        theme_color: '#000000',
+        background_color: '#000000',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'icon.svg',
+            sizes: '192x192 512x512',
+            type: 'image/svg+xml',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
 })
